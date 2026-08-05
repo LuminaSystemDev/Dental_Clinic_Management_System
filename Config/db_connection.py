e5a820e82d33eb2b2713b6cdc5dc07891ebde14d
JoseJf04 <josejafet.dev@gmail.com>
2026-08-05 13:39:04 -0400

    refactor: update of  declarative_base for DeclarativeBase more modern

--- Config/db_connection.py
+++ Config/db_connection.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
 from dotenv import load_dotenv
 import os
 
@@ -32,4 +32,5 @@ SessionLocal = sessionmaker(
 """ Creation to the Base Class for Databse Models """
 
 # Base Class for DB Models
-Base = declarative_base()
+class Base(DeclarativeBase):
+    pass
